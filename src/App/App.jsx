9a98f186349fc1b249220d3184679a7b1fd999d0
Zac Walters <zacwalters4@gmail.com>
2023-02-25 10:59:27 -0700

    Add imports to app.jsx

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react"
 import './App.css'
 import Home from "../Home/Home"
+import AlbumsView from "../AlbumsView/AlbumsView"
+import Album from "../Album/Album"
+import Favorites from "../Favorites/Favorites"
+import Header from "../Header/Header"
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header element={<Header />}/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/albums" element={<AlbumsView />}>
