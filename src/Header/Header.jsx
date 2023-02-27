d15e3297be1f0569fecdfbc37d720e36b0e41929
Zac Walters <zacwalters4@gmail.com>
2023-02-27 10:09:41 -0700

    Add active state to home button

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,12 +1,19 @@
 import './Header.css'
 import { Link } from 'react-router-dom'
-
+import { useLocation } from "react-router-dom"
 
 function Header() {
+    const locationData = useLocation()
+    const activePage = locationData.pathname
+
     return (
       <div className="header">
         <Link to={'/'}>
-            <button className="header-button">Home</button>
+            {(activePage == '/') 
+            ? <button className="header-button active">Home</button>
+            : <button className="header-button">Home</button>
+        }
+            
         </Link>
         <h1>Tune Spoon</h1>
         <Link to={'/favorites'}>
