3dbbeb026f267825d7d21beb9e433e3ef164bb3b
Zac Walters <zacwalters4@gmail.com>
2023-02-27 10:39:51 -0700

    Add api call to album view

--- src/AlbumsView/AlbumsView.jsx
+++ src/AlbumsView/AlbumsView.jsx
@@ -1,6 +1,21 @@
 import './AlbumsView.css'
+import React from 'react'
+import { getAlbums } from '../Utilities/APICalls'
 
 function AlbumsView() {
+    const [albums, storeAlbums] = React.useState([])
+
+    const retrieveAlbums = () => {
+        getAlbums()
+        .then(data => {
+            storeAlbums(data)
+        })
+    }
+
+    React.useEffect(() => {
+        retrieveAlbums()
+    }, [])
+
     return (
       <div className="AlbumsView">
         <h1>hi</h1>
